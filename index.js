80d9ad737754520ad67b679bbf817dc15b117e0a
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-01-21 16:22:00 -0600

    commit tree 82ea06eb1c6e3d9b1cbc72a3a8a44f9e6474a40f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = `bob`
+const leastFavoriteCustomer = `Bob will NEVER be the least favorite customer`
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName)
+}
+//upperCaseCustomerName() //BOB
+
+function setBestCustomer() {
+    bestCustomer = `not bob`
+    console.log(bestCustomer)
+}
+//setBestCustomer() //=> not bob
+
+function overwriteBestCustomer() {
+  bestCustomer =`maybe bob`
+  console.log(bestCustomer)
+}
+//overwriteBestCustomer() //ACTUALLY, BOB IS THE BEST
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = `[IRRELEVANT OPINION] Bob is the least favorite [/IRRELEVANT OPINION]`
+    console.log(leastFavoriteCustomer)
+}
+//changeLeastFavoriteCustomer() //TypeError: Assignment to constant variable
